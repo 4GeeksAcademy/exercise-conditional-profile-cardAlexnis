eedd974c14c6e83cc3a5d11c3e63b8ef9fdf12cc
Alexnis-Herrera <alexnisherrerag@gmail.com>
2025-06-27 10:54:36 +0000

    ProyectoTerminado

--- src/js/app.js
+++ src/js/app.js
@@ -37,14 +37,34 @@ function render(variables = {}) {
     variables.lastName ? variables.lastName : "Last Name"
   } </h1>
           <h2>${variables.role ? variables.role : "Role"}</h2>
-          <h3>${variables.city ? variables.city : "City"}
-          ${variables.country}
+          <h3>${variables.city ? variables.city : "City"} ${
+    variables.country ? variables.country : "Country"
+  }
           </h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href=${
+              variables.twitter ? variables.twitter : "https://x.com/?lang=es"
+            }
+            ><i class="fab fa-twitter"></i></a></li>
+
+            <li><a href=${
+              variables.github ? variables.github : "https://github.com/"
+            } 
+            ><i class="fab fa-github"></i></a></li>
+
+            <li><a href=${
+              variables.linkedin
+                ? variables.linkedin
+                : "https://www.linkedin.com/login/es"
+            }
+            "><i class="fab fa-linkedin"></i></a></li>
+
+            <li><a href=${
+              variables.instagram
+                ? variables.instagram
+                : "https://www.instagram.com/"
+            }
+            ><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
